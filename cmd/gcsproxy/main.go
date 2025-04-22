336afdaa0b684860a5a57988c9f1943e03bbe41a
karupanerura <karupanerura@gmail.com>
2025-04-22 14:01:01 +0900

    feat: add basic auth support

--- cmd/gcsproxy/main.go
+++ cmd/gcsproxy/main.go
@@ -8,8 +8,10 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/karupanerura/gcsproxy"
+	"github.com/tg123/go-htpasswd"
 )
 
 const defaultPort = "8080"
@@ -26,10 +28,25 @@ func main() {
 		log.Printf("failed to create proxy from env: %s", err)
 	}
 
+	handler := proxy
+	if f := parseBasicAuth(getEnv("GCS_PROXY_BASIC_AUTH", "")); f != nil {
+		next := handler
+		handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			username, password, ok := r.BasicAuth()
+			if !ok || !f.Match(username, password) {
+				w.Header().Add("WWW-Authenticate", `Basic realm="Restricted"`)
+				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+
 	port := getEnv("PORT", defaultPort)
 	svr := http.Server{
 		Addr:    net.JoinHostPort("", port),
-		Handler: proxy,
+		Handler: handler,
 	}
 	if err := svr.ListenAndServe(); err == nil || errors.Is(err, http.ErrServerClosed) {
 		return // ok
@@ -52,4 +69,19 @@ func mustEnv(name string) string {
 	}
 
 	panic(fmt.Sprintf("env: %s is required", name))
+}
+
+func parseBasicAuth(s string) *htpasswd.File {
+	if s == "" {
+		return nil
+	}
+
+	f, err := htpasswd.NewFromReader(strings.NewReader(s), htpasswd.DefaultSystems, func(err error) {
+		log.Printf("Invalid GCS_PROXY_BASIC_AUTH: %v", err)
+	})
+	if err != nil {
+		log.Printf("Invalid GCS_PROXY_BASIC_AUTH: %v", err)
+		return nil
+	}
+	return f
 }
--- go.mod
+++ go.mod
@@ -1,13 +1,19 @@
 module github.com/karupanerura/gcsproxy
 
-go 1.18
+go 1.23.0
 
-require cloud.google.com/go/storage v1.27.0
+toolchain go1.24.2
+
+require (
+	cloud.google.com/go/storage v1.27.0
+	github.com/tg123/go-htpasswd v1.2.4
+)
 
 require (
 	cloud.google.com/go v0.104.0 // indirect
 	cloud.google.com/go/compute v1.10.0 // indirect
 	cloud.google.com/go/iam v0.5.0 // indirect
+	github.com/GehirnInc/crypt v0.0.0-20230320061759-8cc1b52080c5 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
@@ -16,10 +22,11 @@ require (
 	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.5.1 // indirect
 	go.opencensus.io v0.23.0 // indirect
-	golang.org/x/net v0.0.0-20221004154528-8021a29435af // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20221006150949-b44042a4b9c1 // indirect
-	golang.org/x/sys v0.0.0-20221006211917-84dc82d7e875 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/api v0.98.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
